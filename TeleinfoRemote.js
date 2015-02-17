902cde0c0e4f08e25c180ddbe3a238c3eca5d8e1
Phil Bri <ph.brioude@gmail.com>
2015-02-17 22:21:17 +0100

    v 0.5

--- TeleinfoRemote.js
+++ TeleinfoRemote.js
@@ -1,21 +1,20 @@
 var tabEco = {	
 	'ISOUSC':'% A',
-	'PPAP':'% VA',
+	'PPAP'	:'% VA',
 	'IINST':'% A',
 	'IINST1':'%-',
 	'IINST2':'%-',
 	'IINST3':'% A',
-	'OPTARIF': 	{'HC..':'HC/HP','EJP.':'EJP','BBRX':'Tempo'},
-	'PTEC':{'TH':'Toutes Heures:#','HP':'Heures Pleines:#','HC':'Heures Creuses:#','HP':'Heures Pleines:#','HN':'Heures Normales:#',
+	'OPTARIF' : {'HC..':'HC/HP','EJP.':'EJP','BBRX':'Tempo'},
+	'PTEC'	: {'TH':'Toutes Heures:#','HP':'Heures Pleines:#','HC':'Heures Creuses:#','HP':'Heures Pleines:#','HN':'Heures Normales:#',
 			'PM':'Pointe:#','HCJB':'Heures Creuses:#00BFFF','HPJB':'Heures Pleines:#00BFFF','HCJW':'Heures Creuses:#FFFFFF',
 			'HPJW':'Heures Pleines:#FFFFFF','HCJR':'Heures Creuses:#FF0000','HPJR':'Heures Pleines:#FF0000'},
-	'DEMAIN':{'----':'indisponible:-----:#FAF0E6','Bleu':'Bleu:#00BFFF','Rouge':'Rouge:#FF0000','Blanc':'Blanc:#FFFFFF'},
-	'HHPHC':'%'},
-	cfg;
+	'DEMAIN': {'----':'indisponible:-----:#','Bleu':'Bleu:#00BFFF','Rouge':'Rouge:#FF0000','Blanc':'Blanc:#FFFFFF'},
+	'HHPHC' : '%'},cfg;
 
 exports.init = function ( SARAH ) {
 	SARAH.context.TeleinfoRemote= {'numCpt': 'Compteur 1'};
-	console.log ('\nTeleinfoRemote[OK] ====> init <====\n');
+	console.log ('\nTeleinfoRemote [OK] ====> init <====\n');
 	cfg = SARAH.ConfigManager.getConfig().modules.TeleinfoRemote; 
 	SARAH.call('TeleinfoRemote',{"Host":cfg.Host,"User":cfg.User,"Password":cfg.Password});
 }
@@ -34,26 +33,25 @@ exports.action = function ( data , callback , config , SARAH ) {
 		var str = '';
 		var tabExp = {};
 
-		response.on ( 'data', function ( chunk ) {
-			str += chunk;
-		});
+		response.on ( 'data', function ( chunk ) { str += chunk; });
 
 		response.on ( 'end', function () {
 			for( var key in tabEco ) {
 				reg = new RegExp ( '<' + cpt + '_'+ key + '>(.*?)</' + cpt + '_' + key + '>',"gm" ).exec(str)[1];
 
-				if (typeof tabEco[key] != 'object') tabExp[key] = tabEco[key].replace ('%', reg);
+				if ( typeof tabEco[key] != 'object') tabExp[key] = tabEco[key].replace ( '%', reg );
 				else {
 					tabExp[key] = tabEco[key][reg];
 					reg = tabEco[key][reg];
 				}
 				if ( key == data.tag ) {
-					callback({'tts': data.ttsAction.replace( '%', reg.split(':').shift() )});
-					console.log ( '\nTeleinfoRemote [OK] => ' + data.ttsAction.replace('%',reg) );
+					callback({ 'tts': data.ttsAction.replace( '%', reg.split(':').shift() )});
+					console.log ( '\nTeleinfoRemote [OK] => ' + data.ttsAction.replace( '%' ,reg ) );
 				}
 			}
-			SARAH.context.TeleinfoRemote.tab = tabExp;			
+			SARAH.context.TeleinfoRemote.tab = tabExp;
 			callback({'tts':JSON.stringify(SARAH.context.TeleinfoRemote)});
+			if  (data.ttsAction!='undefined' ) console.log( '\nTeleinfoRemote [OK] => Mise à jour des infos envoyée...' );
 		});
 	}
 	require ('http').request('http://'+cfg.User+':'+cfg.Password+'@'+cfg.Host+'/protect/settings/teleinfo' + cpt[1] + '.xml', clbk).end();
