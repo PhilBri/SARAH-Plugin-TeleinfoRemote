fc73464f7b3ff12e14cc1d5b7e15a02e6a08eb0c
Phil Bri <ph.brioude@gmail.com>
2015-02-17 11:31:17 +0100

    V 0.4

--- TeleinfoRemote.js
+++ TeleinfoRemote.js
@@ -1,60 +1,71 @@
-function sendEco ( url, retCmd ) {
-	var http = require ( 'http' );
-	var str = '';
-	http.get ( url, function( res ) {
-		res.on ( 'data', function ( chunk ) {
-			str += chunk;
-		});
-		res.on ( 'end', function () {
-			retCmd ( str );
-		});
-	}).on ( 'error', function ( error ) {
-		console.log ( '\nTeleinfoRemote [erreur] => ' + error.message );
-	});
-}
+var tabEco = {	
+	'ISOUSC':'% A',
+	'PPAP':'% VA',
+	'IINST':'% A',
+	'IINST1':'%-',
+	'IINST2':'%-',
+	'IINST3':'% A',
+	'OPTARIF': 	{'HC..':'HC/HP','EJP.':'EJP','BBRX':'Tempo'},
+	'PTEC':{'TH':'Toutes Heures','HP':'Heures Pleines','HC':'Heures Creuses','HP':'Heures Pleines','HN':'Heures Normales',
+			'PM':'Pointe','HCJB':'Heures Creuses:#00BFFF','HPJB':'Heures Pleines:#00BFFF','HCJW':'Heures Creuses:#FFFFFF',
+			'HPJW':'Heures Pleines:#FFFFFF','HCJR':'Heures Creuses:#FF0000','HPJR':'Heures Pleines:#FF0000'},
+	'DEMAIN':{'----':'indisponible:-----:#FAF0E6','Bleu':'Bleu:#00BFFF','Rouge':'Rouge:#FF0000','Blanc':'Blanc:#FFFFFF'},
+	'HHPHC':'%'},
+	cfg;
 
 exports.init = function ( SARAH ) {
-
-	if ( !exports.tarif ) exports.tarif = 'Demandez à SARAH' ;
-	if ( !exports.conso ) exports.conso = 'Demandez à SARAH';
+	SARAH.context.TeleinfoRemote= {'numCpt': 'Compteur 1'};
+	console.log ('\nTeleinfoRemote[OK] ====> init <====\n');
+	cfg = SARAH.ConfigManager.getConfig().modules.TeleinfoRemote; 
+	SARAH.call('TeleinfoRemote',{"Host":cfg.Host,"User":cfg.User,"Password":cfg.Password});
 }
 
 exports.action = function ( data , callback , config , SARAH ) {
-	var	cfg 	= config.modules.TeleinfoRemote;
-	var ecoUrl = 'http://' + cfg.User + ':' + cfg.Password + '@' + cfg.Host + '/teleinfo.xml'; // ou api/xdevices.json
+	if (data.cpt) SARAH.context.TeleinfoRemote.numCpt = data.cpt;
+	SARAH.context.TeleinfoRemote.numCpt == 'Compteur 1' ? cpt = 'T1' : cpt = 'T2';
+	SARAH.context.TeleinfoRemote.maj = require('moment')().format('HH[h]:mm');
 
 	if ( !cfg.Host || !cfg.User || !cfg.Password ) {
 		console.log ( "\nTeleinfoRemote [Erreur] => Hote ou User / Password non paramétré !" );
 		return callback ({ 'tts' : 'Erreur de configuration' });
 	}
 
-	sendEco( ecoUrl, function ( retCmd ) {
-		var str = new RegExp ( '<' + data.tag + '>(.*?)<\/' + data.tag + '>', 'gm' ).exec( retCmd )[1];
+	clbk = function( response ) {
+		var str = '';
+		var tabExp = {};
 
-		switch ( str ) {
-			case 'HC' :
-				exports.tarif = 'Heures Creuses';
-				data.ttsAction = data.ttsAction.replace ( /%/, exports.tarif);
-				break;
-			case 'HP' :
-				exports.tarif = 'Heures Pleines';
-				data.ttsAction = data.ttsAction.replace ( /%/, exports.tarif);
-				break;
-			case '0' :
-					SARAH.speak ( 'Vous êtes en triphasé');
-					str = new RegExp ( '<' + data.tag + 1 + '>(.*?)<\/' + data.tag + 1 + '>', 'gm' ).exec( retCmd )[1];
-					data.ttsAction = data.ttsAction.replace( /%/, str);
-					data.ttsAction += ' sur la phase 1, ';
-					str = new RegExp ( '<' + data.tag + 2 + '>(.*?)<\/' + data.tag + 2 + '>', 'gm' ).exec( retCmd )[1];
-					data.ttsAction += str + ' ampère sur la phase 2, ';
-					str = new RegExp ( '<' + data.tag + 3 + '>(.*?)<\/' + data.tag + 3 + '>', 'gm' ).exec( retCmd )[1];
-					data.ttsAction += str + ' ampère sur la phase 3';
-				break;
-			default :
-				if ( data.tag == 'T1_PPAP' ) exports.conso = str + ' VA';
-				data.ttsAction = data.ttsAction.replace ( /%/, str);
-		}
-		console.log ( '\nTeleinfoRemote [OK] => ' + data.ttsAction );
-		callback ({ 'tts' : data.ttsAction });
-	});
+		response.on ( 'data', function ( chunk ) {
+			str += chunk;
+		});
+
+		response.on ( 'end', function () {
+			for( var key in tabEco ) {
+				reg = new RegExp ( '<' + cpt + '_'+ key + '>(.*?)</' + cpt + '_' + key + '>',"gm" ).exec(str)[1];
+
+				if (typeof tabEco[key] != 'object') tabExp[key] = tabEco[key].replace ('%', reg);
+				else {
+					tabExp[key] = tabEco[key][reg];
+					reg = tabEco[key][reg];
+				}
+				if ( key == data.tag ) {
+					callback({'tts': data.ttsAction.replace( '%', reg.split(':').shift() )});
+					console.log ( '\nTeleinfoRemote [OK] => ' + data.ttsAction.replace('%',reg) );
+				}
+			}
+			SARAH.context.TeleinfoRemote.tab = tabExp;
+			// tests
+			SARAH.context.TeleinfoRemote.tab.PTEC = 'Heures Creuses:#00BFFF';
+			//SARAH.context.TeleinfoRemote.tab.OPTARIF = 'Tempo';
+			SARAH.context.TeleinfoRemote.tab.DEMAIN = 'Bleu:#00BFFF'
+			//SARAH.context.TeleinfoRemote.tab.IINST = '10 A';
+			//SARAH.context.TeleinfoRemote.numCpt = 'Compteur 2';
+			// tests end
+			console.log('\nValeurs :........................\n');
+			console.log(SARAH.context.TeleinfoRemote);
+			
+			callback({'tts':JSON.stringify(SARAH.context.TeleinfoRemote)});
+		});
+	}
+	require ('http').request('http://'+cfg.User+':'+cfg.Password+'@'+cfg.Host+'/protect/settings/teleinfo' + cpt[1] + '.xml', clbk).end();
+
 }
