1bf354dd9c64c649a61840e1d8a54199c51236f4
Phil Bri <ph.brioude@gmail.com>
2015-02-24 01:25:30 +0100

    v 0.6

--- TeleinfoRemote.js
+++ TeleinfoRemote.js
@@ -19,9 +19,10 @@ exports.init = function ( SARAH ) {
 }
 
 exports.action = function ( data , callback , config , SARAH ) {
-	//var numCpt;
-	data.cpt ? numCpt = data.cpt : numCpt = 'Compteur 1'; 
- 	numCpt == 'Compteur 1' ? cpt = 'T1' : cpt = 'T2'; 
+	var numCpt ;
+	SARAH.context.TeleinfoRemote===undefined ? numCpt ='Compteur 1' : numCpt = SARAH.context.TeleinfoRemote.numCpt;
+	if (data.cpt!==undefined) numCpt = data.cpt;
+ 	numCpt == 'Compteur 1' ? cpt = 'T1' : cpt = 'T2';
 
 	if ( !cfg.Host || !cfg.User || !cfg.Password ) {
 		console.log ( "\nTeleinfoRemote [Erreur] => Hote ou User / Password non paramétré !" );
@@ -36,23 +37,35 @@ exports.action = function ( data , callback , config , SARAH ) {
 
 		response.on ( 'end', function () {
 			for( var key in tabDev ) {
-				reg = new RegExp ( '<' + cpt + '_'+ key + '>(.*?)</' + cpt + '_' + key + '>',"gm" ).exec(str)[1];
+				reg = new RegExp ( '<' + cpt + '_'+ key + '>(.*?)</' + cpt + '_' + key + '>',"gm" ).exec(str);
+				if (reg) reg=reg[1];
 
 				if ( typeof tabDev[key] != 'object') tabPlug[key] = tabDev[key].replace ( '%', reg );
 				else {
 					tabPlug[key] = tabDev[key][reg];
 					reg = tabDev[key][reg];
 				}
-				if ( key == data.tag ) {
-					callback({ 'tts': data.ttsAction.replace( '%', reg.split(':').shift() )});
-					console.log ( '\nTeleinfoRemote [OK] => ' + data.ttsAction.replace( '%' ,reg ) );
-				}
+				if ( key == data.tag ) data.ttsAction = data.ttsAction.replace( '%', reg.split(':').shift() );
 			}
+			if (data.ttsAction) {
+				callback({'tts':data.ttsAction});
+				console.log('\nTeleinfoRemote [OK] => ' + data.ttsAction);
+			} else console.log( '\nTeleinfoRemote [OK] => Mise à jour portlet...' );
+
 			SARAH.context.TeleinfoRemote= { 'numCpt':numCpt, 'maj': new Date(), 'tab': tabPlug };
 
+			// tests
+			//SARAH.context.TeleinfoRemote.tab.PTEC = 'Heures Pleines:#FF0000';
+			//SARAH.context.TeleinfoRemote.tab.OPTARIF = 'EJP';
+			//SARAH.context.TeleinfoRemote.tab.DEMAIN = 'Rouge:#FF0000';
+			//SARAH.context.TeleinfoRemote.tab.PEJP = '30';
+			//SARAH.context.TeleinfoRemote.numCpt = 'Compteur 2';
+			// tests end
+			
 			if (data.need) SARAH.speak(data.need);
 			callback({'tts':JSON.stringify(SARAH.context.TeleinfoRemote)});
-			if ( data.ttsAction!='undefined' ) console.log( '\nTeleinfoRemote [OK] => Mise à jour portlet...' );
+			console.log('\nValeurs :........................\n');
+			console.log(SARAH.context.TeleinfoRemote);
 		});
 	}
 	require ('http').request('http://'+cfg.User+':'+cfg.Password+'@'+cfg.Host+'/protect/settings/teleinfo' + cpt[1] + '.xml', clbk).end();
